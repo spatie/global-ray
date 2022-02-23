2e78accb537e9895d888567a03b5f8776b976662
freekmurze <freekmurze@users.noreply.github.com>
2022-02-23 21:56:15 +0000

    Fix styling

--- tests/BuildTest.php
+++ tests/BuildTest.php
@@ -4,16 +4,16 @@ use Spatie\GlobalRay\Support\Ray;
 use Symfony\Component\Process\Process;
 
 it('build the phar', function () {
-   $process = executeCommand('./bin/global-ray build');
+    $process = executeCommand('./bin/global-ray build');
 
-   expect($process->isSuccessful())->toBeTrue();
+    expect($process->isSuccessful())->toBeTrue();
 
-   expect(Ray::getPharPath())->toBeFile();
+    expect(Ray::getPharPath())->toBeFile();
 });
 
 function executeCommand(string $command): Process
 {
-    $process =  Process::fromShellCommandline($command);
+    $process = Process::fromShellCommandline($command);
 
     $process->run();
 
