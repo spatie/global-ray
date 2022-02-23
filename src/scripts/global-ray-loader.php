729671db96f940fac16fcae081fba51938815b5b
Freek Van der Herten <freek@spatie.be>
2022-02-23 22:16:13 +0100

    ignore all errors

--- src/scripts/global-ray-loader.php
+++ src/scripts/global-ray-loader.php
@@ -1,33 +1,37 @@
 <?php
 
-preg_match("#^\d.\d#", PHP_VERSION, $match);
+try {
+    preg_match("#^\d.\d#", PHP_VERSION, $match);
 
-$version = $match[0];
+    $version = $match[0];
 
-$pharPath = realpath(__DIR__ . "/../../ray-phars/ray_php_{$version}.phar");
+    $pharPath = realpath(__DIR__ . "/../../ray-phars/ray_php_{$version}.phar");
 
-$packages = [
-    'spatie/ray',
-    'spatie/yii-ray',
-    'spatie/craft-ray',
-    'spatie/laravel-ray',
-    'spatie/wordpress-ray',
-];
+    $packages = [
+        'spatie/ray',
+        'spatie/yii-ray',
+        'spatie/craft-ray',
+        'spatie/laravel-ray',
+        'spatie/wordpress-ray',
+    ];
 
-$composerJson = getcwd() . '/composer.json';
+    $composerJson = getcwd() . '/composer.json';
 
-if (file_exists($composerJson)) {
-    $composer = json_decode(file_get_contents($composerJson), true);
+    if (file_exists($composerJson)) {
+        $composer = json_decode(file_get_contents($composerJson), true);
 
-    foreach (['require', 'require-dev'] as $require) {
-        foreach ($composer[$require] ?? [] as $package => $version) {
-            if (in_array($package, $packages)) {
-                return;
+        foreach (['require', 'require-dev'] as $require) {
+            foreach ($composer[$require] ?? [] as $package => $version) {
+                if (in_array($package, $packages)) {
+                    return;
+                }
             }
         }
     }
-}
 
-if (file_exists($pharPath)) {
-    require_once $pharPath;
+    if (file_exists($pharPath)) {
+        require_once $pharPath;
+    }
+} catch (Exception $exception) {
+
 }
