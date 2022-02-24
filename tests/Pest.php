5df48d26aa0148cc0b5fc3c256fb0bda005c61a4
Steve Bauman <steven_bauman@outlook.com>
2022-02-23 23:25:24 -0500

    Scaffold temp php ini for each test

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,6 +2,14 @@
 
 use Symfony\Component\Process\Process;
 
+beforeEach(function () {
+    file_put_contents(getIniPath(), '');
+});
+
+afterEach(function () {
+    unlink(getIniPath());
+});
+
 function executeCommand(string $command): Process
 {
     $process = Process::fromShellCommandline($command);
