a6a1a62354adf864920ee5dd615d561adf591502
Steve Bauman <steven_bauman@outlook.com>
2022-02-28 16:47:54 -0500

    Revert patchers change

--- dump-phar-generator/scoper.inc.php
+++ dump-phar-generator/scoper.inc.php
@@ -3,16 +3,4 @@
 return [
     'prefix' => 'GlobalDump',
     'expose-functions' => ['dump', 'dd'],
-    'patchers' => [
-        function (string $filePath, string $prefix, string $content) {
-            if (strpos($filePath, 'composer.json') !== false) {
-                $json = json_decode($content, true);
-                $json['config']['autoloader-suffix'] = 'GlobalDump';
-
-                return json_encode($json);
-            }
-
-            return $content;
-        },
-    ],
 ];
--- ray-phar-generator/scoper.inc.php
+++ ray-phar-generator/scoper.inc.php
@@ -3,16 +3,4 @@
 return [
     'prefix' => 'GlobalRay',
     'expose-functions' => ['ray', 'rd'],
-    'patchers' => [
-        function (string $filePath, string $prefix, string $content) {
-            if (strpos($filePath, 'composer.json') !== false) {
-                $json = json_decode($content, true);
-                $json['config']['autoloader-suffix'] = 'GlobalRay';
-
-                return json_encode($json);
-            }
-
-            return $content;
-        },
-    ],
 ];
