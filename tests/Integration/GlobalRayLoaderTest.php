8c941b784ad0a336cbca8219f6818b2f985fc5b2
Steve Bauman <steven_bauman@outlook.com>
2022-03-02 12:44:35 -0500

    Add rd tests

--- tests/Integration/GlobalRayLoaderTest.php
+++ tests/Integration/GlobalRayLoaderTest.php
@@ -8,6 +8,14 @@ it('will resolve ray global function', function () {
     expect(function_exists('ray'))->toBeTrue();
 });
 
-it('will not generate exceptions', function () {
+it('will resolve rd global function', function () {
+    expect(function_exists('rd'))->toBeTrue();
+});
+
+it('will not generate exceptions when calling ray', function () {
     ray('foo');
+})->expectNotToPerformAssertions();
+
+it('will not generate exceptions when calling rd', function () {
+    ray('rd');
 })->expectNotToPerformAssertions();
