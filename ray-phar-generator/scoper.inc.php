ab718bdc5a8b6966ba65be1df3ba6580c3f6180d
Steve Bauman <steven_bauman@outlook.com>
2025-02-24 18:48:59 -0500

    Don't expose globals

--- ray-phar-generator/scoper.inc.php
+++ ray-phar-generator/scoper.inc.php
@@ -3,4 +3,7 @@
 return [
     'prefix' => 'GlobalRay',
     'expose-functions' => ['ray', 'rd'],
+    'expose-global-constants' => false,
+    'expose-global-classes' => false,
+    'expose-global-functions' => false,
 ];
