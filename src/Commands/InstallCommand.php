28bf011dc5ed77b5b8516ce02871b655d647c37b
Dave Rogers <redcore@gmail.com>
2026-08-07 00:55:28 -0700

    Support void return type for Symfony configure method (#83)

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -16,7 +16,7 @@ class InstallCommand extends Command
     use ConfirmsPhpIniPath;
     use RetriesAsWindowsAdmin;
 
-    protected function configure()
+    protected function configure(): void
     {
         if (method_exists($this, 'setName')) {
             $this->setName('install');
